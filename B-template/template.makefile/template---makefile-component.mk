6bd1425aa7b19fff952276e8eea075e719f67e6a
Matthew Yan <matthew8749@users.noreply.github.com>
2025-08-13 10:54:05 +0800

    Create template---makefile-component.mk

---
+++ B-template/template.makefile/template---makefile-component.mk
@@ -0,0 +1,30 @@
+# ======================================================================================
+******************  有註解  ******************
+
+help:  # <--- 定義 help 這個 target
+  @printf "Available targets\n"  # 印出表頭
+  @awk '/^[a-zA-Z\-\_0-9]+:/ { \      # 搜尋以 target 名稱開頭的行（例如 build:）
+    helpMessage = match(lastLine, /^## (.*)/); \  # 如果前一行有以 ## 開頭的註解
+    if (helpMessage) { \
+      helpCommand = substr($$1, 0, index($$1, ":")-1); \  # 擷取 target 名稱
+      helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \  # 擷取說明文字
+      printf "%-15s %s\n", helpCommand, helpMessage; \  # 輸出格式化資訊
+    } \
+  } \
+  { lastLine = $$0 }' $(MAKEFILE_LIST)  # 記住當前行，下一行用來檢查
+
+******************  沒註解  ******************
+
+help:
+  @printf "Available targets\n"
+  @awk '/^[a-zA-Z\-\_0-9]+:/ { \
+    helpMessage = match(lastLine, /^## (.*)/); \
+    if (helpMessage) { \
+      helpCommand = substr($$1, 0, index($$1, ":")-1); \
+      helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+      printf "%-15s %s\n", helpCommand, helpMessage; \
+    } \
+  } \
+  { lastLine = $$0 }' $(MAKEFILE_LIST)  # 記住當前行，下一行用來檢查
+
+  # ======================================================================================
